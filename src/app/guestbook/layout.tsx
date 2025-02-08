224bad6c134e7d7bee262a73a86c54760f7c1918
karthik-bha <143622670+karthik-bha@users.noreply.github.com>
2025-02-08 23:38:02 +0530

    ESLint issue fixed

--- src/app/guestbook/layout.tsx
+++ src/app/guestbook/layout.tsx
@@ -1,15 +1,7 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
-import { Session } from "next-auth";
-export default function RootLayout({
-  children,
-  session, // Accept session prop
-}: {
-  children: React.ReactNode;
-  session: Session | null; 
-}) {
-  return (
-    <SessionProvider session={session}>{children}</SessionProvider>
-  );
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return <SessionProvider>{children}</SessionProvider>;
 }
